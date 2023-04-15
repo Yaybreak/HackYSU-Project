3aa0695d64ec5450ae2ee30ec7f4635dd748272c
Yaybreak <130863943+Yaybreak@users.noreply.github.com>
2023-04-15 14:43:25 -0400

    bigmean commit

---
+++ admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Admin</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+</head>
+
+
+
+<body>
+  <header>
+<h1> Admin </h1>
+
+  </header>
+  <main>
+ 
+<ul> 
+	<a href="login.php">Login</a> <br>
+</ul>
+
+
+  </main>
+</body>
+
+
+</html>
